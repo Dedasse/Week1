2dba3af1a3fea63ed7b10c2ecc8ba3fe097240ac
mikaely <mikael.ylivaara@metropolia.fi>
2020-03-19 14:33:52 +0200

    a

--- server-one/index.js
+++ server-one/index.js
@@ -7,4 +7,13 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    'name': 'Frank',
+    'age': 6,
+    'weight': 5,
+  };
+  res.json(cat);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
