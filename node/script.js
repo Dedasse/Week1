fc5b8ff6a277792c97d943a6a11ee71ad6e65764
mikaely <mikael.ylivaara@metropolia.fi>
2020-03-22 14:44:15 +0200

    last

--- node/script.js
+++ node/script.js
@@ -1,2 +1,10 @@
 'use strict'
-console.log("This is JavaScript point ");
+const _=require('lodash')
+
+
+const argh= _.camelCase('FooBar');
+
+console.log("This is JavaScript");
+console.log("lodash "+argh);
+
+
